63de1d34e80bff3f3f52cbf69829f8a7f0799f97
Nikita Shpak <eqlbin@yandex.ru>
2017-03-09 13:29:25 +1200

    Example updated

--- example/src/ru/eqlbin/gwt/datepicker/example/client/ListBoxDatePickerExample.java
+++ example/src/ru/eqlbin/gwt/datepicker/example/client/ListBoxDatePickerExample.java
@@ -16,6 +16,7 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 import ru.eqlbin.gwt.datepicker.client.ListBoxDatePicker;
 
@@ -24,80 +25,70 @@ import ru.eqlbin.gwt.datepicker.client.ListBoxDatePicker;
  */
 public class ListBoxDatePickerExample implements EntryPoint {
     
+    
+    private static final DateTimeFormat DATE_FORMAT = DateTimeFormat.getFormat("yyyy-MM-dd");
 
     /**
      * This is the entry point method.
      */
     public void onModuleLoad() {
-
-        final ListBoxDatePicker datePicker = new ListBoxDatePicker();
-
-        final Label firstYearLabel = new Label("Min year / Current year:"); 
-        final Label lastYearLabel = new Label("Max year / Years count:"); 
+        RootPanel.get("datePickerFloating").add(createFloatingDatePickerExample());
+        RootPanel.get("datePickerFixed").add(createFixedDatePickerExample());
+    }
+    
+    @SuppressWarnings("deprecation")
+    public Widget createFloatingDatePickerExample() {
+        
+        int currentYear = new Date().getYear() + 1900;
+        int yearsCount = 10;
+        
+        final ListBoxDatePicker datePickerFloating = new ListBoxDatePicker();
+        datePickerFloating.setFloatingYearsRange(currentYear, yearsCount);
+        
+        final Label currentYearLabel = new Label("Current year:"); 
+        final Label yearsCountLabel = new Label("Years count:"); 
         final Label selectedDateLabel = new Label();
-        final TextBox firstYearBox = new TextBox();
-        final TextBox lastYearBox = new TextBox();
-        final Button fixedRangeButton = new Button("Set fixed range");
-        final Button floatingRangeButton = new Button("Set floating range");
-
-        final DateTimeFormat dateFormat = DateTimeFormat.getFormat("yyyy-MM-dd");
-
+        
+        final TextBox currentYearInputBox = new TextBox();
+        currentYearInputBox.setText(String.valueOf(currentYear));
+        final TextBox yearCountInput = new TextBox();
+        yearCountInput.setText(String.valueOf(yearsCount));
+        
+        final Button setRangeButton = new Button("Set range");
+        
         VerticalPanel datePickerPanel = new VerticalPanel();
-        datePickerPanel.
-            setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
-        datePickerPanel.setWidth("100%");
+        datePickerPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+        datePickerPanel.setWidth("300px");
         datePickerPanel.setSpacing(5);
-        datePickerPanel.add(datePicker);
+        datePickerPanel.add(datePickerFloating);
         datePickerPanel.add(selectedDateLabel);
-        datePickerPanel.
-            setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+        datePickerPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
         
-        HorizontalPanel buttonPanel = new HorizontalPanel();
-        buttonPanel.setSpacing(5);
-        buttonPanel.add(fixedRangeButton);
-        buttonPanel.add(floatingRangeButton);
-        
-        VerticalPanel yearsRangeChanger = new VerticalPanel();
-        yearsRangeChanger.setWidth("100%");
-        yearsRangeChanger.
-            setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
-        yearsRangeChanger.setSpacing(2);
-        yearsRangeChanger.add(firstYearLabel);
-        yearsRangeChanger.add(firstYearBox);
-        yearsRangeChanger.add(lastYearLabel);
-        yearsRangeChanger.add(lastYearBox);
-        yearsRangeChanger.add(buttonPanel);
+        VerticalPanel yearsRangePanel = new VerticalPanel();
+        yearsRangePanel.setWidth("300px");
+        yearsRangePanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
+        yearsRangePanel.setSpacing(5);
+        yearsRangePanel.add(currentYearLabel);
+        yearsRangePanel.add(currentYearInputBox);
+        yearsRangePanel.add(yearsCountLabel);
+        yearsRangePanel.add(yearCountInput);
+        yearsRangePanel.add(setRangeButton);
         
         HorizontalPanel mainPanel = new HorizontalPanel();
         mainPanel.setWidth("100%");
         mainPanel.setSpacing(10);
         mainPanel.add(datePickerPanel);
-        mainPanel.add(yearsRangeChanger);
-
-        RootPanel.get("datePicker").add(mainPanel);
-
-        fixedRangeButton.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {               
-                int minYear = Integer.parseInt(firstYearBox.getValue());
-                int maxYear = Integer.parseInt(lastYearBox.getValue());
+        mainPanel.add(yearsRangePanel);
+        mainPanel.setCellHorizontalAlignment(datePickerPanel, HasHorizontalAlignment.ALIGN_RIGHT);
+        mainPanel.setCellHorizontalAlignment(yearsRangePanel, HasHorizontalAlignment.ALIGN_LEFT);
                 
-                if(minYear <= 0 || maxYear <= 0 || minYear > maxYear ) {
-                    Window.alert("Wrong fixed range of years!" );
-                    return;
-                }
-                
-                datePicker.setFixedYearsRange(minYear, maxYear);
-            }
-        });
-        
-        floatingRangeButton.addClickHandler(new ClickHandler() {
+        setRangeButton.addClickHandler(new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
                 
-                int currentYear = Integer.parseInt(firstYearBox.getValue());
-                int yearsCount = Integer.parseInt(lastYearBox.getValue());
-
+                int currentYear = Integer.parseInt(currentYearInputBox.getValue());
+                int yearsCount = Integer.parseInt(yearCountInput.getValue());
+                
                 if(currentYear < 0) {
                     Window.alert("Current year must be > -1!");
                     return;
@@ -108,16 +99,92 @@ public class ListBoxDatePickerExample implements EntryPoint {
                     return;
                 }
                 
-                datePicker.setFloatingYearsRange(currentYear, yearsCount);
+                datePickerFloating.setFloatingYearsRange(currentYear, yearsCount);
             }
         });
-
-        datePicker.addValueChangeHandler(new ValueChangeHandler<Date>() {
+        
+        datePickerFloating.addValueChangeHandler(new ValueChangeHandler<Date>() {
             @Override
             public void onValueChange(ValueChangeEvent<Date> event) {
                 selectedDateLabel.setText("Selected date: " +
-                        dateFormat.format(event.getValue()));
+                        DATE_FORMAT.format(event.getValue()));
             }
         });
+        
+        return mainPanel;
     }
+    
+    @SuppressWarnings("deprecation")
+    private Widget createFixedDatePickerExample() {
+        
+        int currentYear = new Date().getYear() + 1900;
+        int minYear = currentYear - 5;
+        int maxYear = currentYear + 5;
+        
+        final ListBoxDatePicker datePickerFixed = new ListBoxDatePicker();
+        datePickerFixed.setFixedYearsRange(minYear, maxYear);
+        
+        final Label minYearLabel = new Label("Min year:"); 
+        final Label maxYearLabel = new Label("Max year:"); 
+        final Label selectedDateLabel = new Label();
+        
+        final TextBox minYearInput = new TextBox();
+        minYearInput.setText(String.valueOf(minYear));
+        final TextBox maxYearInput = new TextBox();
+        maxYearInput.setText(String.valueOf(maxYear));
+        
+        final Button setRangeButton = new Button("Set range");
+
+        VerticalPanel datePickerPanel = new VerticalPanel();
+        datePickerPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+        datePickerPanel.setWidth("300px");
+        datePickerPanel.setSpacing(5);
+        datePickerPanel.add(datePickerFixed);
+        datePickerPanel.add(selectedDateLabel);
+        datePickerPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+
+        VerticalPanel yearsRangePanel = new VerticalPanel();
+        yearsRangePanel.setWidth("300px");
+        yearsRangePanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
+        yearsRangePanel.setSpacing(5);
+        yearsRangePanel.add(minYearLabel);
+        yearsRangePanel.add(minYearInput);
+        yearsRangePanel.add(maxYearLabel);
+        yearsRangePanel.add(maxYearInput);
+        yearsRangePanel.add(setRangeButton);
+        
+        HorizontalPanel mainPanel = new HorizontalPanel();
+        mainPanel.setWidth("100%");
+        mainPanel.setSpacing(10);
+        mainPanel.add(datePickerPanel);
+        mainPanel.add(yearsRangePanel);
+        mainPanel.setCellHorizontalAlignment(datePickerPanel, HasHorizontalAlignment.ALIGN_RIGHT);
+        mainPanel.setCellHorizontalAlignment(yearsRangePanel, HasHorizontalAlignment.ALIGN_LEFT);
+        
+        setRangeButton.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {               
+                int minYear = Integer.parseInt(minYearInput.getValue());
+                int maxYear = Integer.parseInt(maxYearInput.getValue());
+                
+                if(minYear <= 0 || maxYear <= 0 || minYear > maxYear ) {
+                    Window.alert("Wrong fixed range of years!" );
+                    return;
+                }
+                
+                datePickerFixed.setFixedYearsRange(minYear, maxYear);
+            }
+        });
+
+        datePickerFixed.addValueChangeHandler(new ValueChangeHandler<Date>() {
+            @Override
+            public void onValueChange(ValueChangeEvent<Date> event) {
+                selectedDateLabel.setText("Selected date: " +
+                        DATE_FORMAT.format(event.getValue()));
+            }
+        });
+        
+        return mainPanel;
+    }
+    
 }
